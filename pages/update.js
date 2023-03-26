def554b4062a268a9940df5aa268dbfd11bfa8c3
handanawilliyantoro <handanawilli0902@gmail.com>
2023-03-27 02:37:47 +0700

    fix: add ui update

--- pages/update.js
+++ pages/update.js
@@ -26,6 +26,11 @@ const content = [
     date: '23/03/2023',
     title: 'New Persona on Chat Feature',
     description: "Add Mental Health Adviser, DIY Expert and Chef Persona."
+  },
+  {
+    date: '26/03/2023',
+    title: 'Hotfix UI on Dark Mode Screen',
+    description: "Bringing clarity to the darkness of your screen."
   }
 ]
 
