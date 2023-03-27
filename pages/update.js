8e7b07fcde94c1ce7b3811acb902dde29354b7e6
handanawilliyantoro <handanawilli0902@gmail.com>
2023-03-27 19:46:10 +0700

    fix: update feat

--- pages/update.js
+++ pages/update.js
@@ -36,15 +36,15 @@ const content = [
 
 const Update = () => {
   return (
-    <div className='max-w-screen-md border-2 h-screen m-auto flex flex-col'>
+    <div className='max-w-screen-md  border-2 h-screen m-auto flex flex-col'>
       <Header />
-      <div className='flex flex-col h-full p-4 py-0 overflow-y-scroll'>
-        <p className='text-sm font-bold text-center ml-4 mt-6 mb-4 font-sans'>Stay ahead with our latest update!</p>
+      <div className='flex flex-col h-full p-4 py-0 overflow-y-scroll bg-white'>
+        <p className='text-sm font-bold text-center ml-4 mt-6 mb-4 font-sans text-black'>Stay ahead with our latest update!</p>
         {content.map(a => (
-          <div className='flex flex-row items-center justify-center w-full border-2 p-4 py-3 my-2 rounded max-md:p-2'>
-            <p className='font-bold bg-green-200 mr-4 font-sans text-center py-2 text-xs flex-[0.15] rounded max-md:mr-4 max-md:px-2'>{a.date}</p>
+          <div className='flex flex-row border-gray-200 items-center justify-center bg-white w-full border-2 p-4 py-3 my-2 rounded max-md:p-2'>
+            <p className='font-bold bg-green-200 mr-4 font-sans text-center py-2 text-xs flex-[0.15] text-black rounded max-md:mr-4 max-md:px-2'>{a.date}</p>
             <div className='flex flex-col items-start ml-4 flex-[0.85] justify-start max-md:ml-0'>
-              <h4 className='font-serif font-bold text-sm mb-2'>{a.title}</h4>
+              <h4 className='font-serif font-bold text-sm text-black mb-2'>{a.title}</h4>
               <p className='text-xs text-gray-500 font-serif'>{a.description}</p>
             </div>
           </div>
