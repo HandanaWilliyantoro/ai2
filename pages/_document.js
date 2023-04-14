84c37f9b434e1ee46f0a8eb542165f8e4f2cb617
handanawilliyantoro <handanawilli0902@gmail.com>
2023-04-14 23:38:43 +0700

    fix: adsense script placement

--- pages/_document.js
+++ pages/_document.js
@@ -3,13 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6332345664092406"
+        crossorigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6332345664092406"
-      crossorigin="anonymous"></script>
     </Html>
   )
 }
