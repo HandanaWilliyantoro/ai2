998d0ffd4c630980ec16f4b1e2371203cceef118
handanawilliyantoro <handanawilli0902@gmail.com>
2023-04-15 02:59:26 +0700

    fix: revert changes

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,5 +1,3 @@
-import Ads from "./ads.json"
-
 export default async function handler(req, res) {
   try {
     let {q} = req.body;
@@ -18,23 +16,6 @@ export default async function handler(req, res) {
     const sources = await engine.data.slice(0, 5)
 
     const parsedSources = await sources.map((a, i) => `Sources [${i+1}]: ${a.snippet}`)
-
-    let relatedAds;
-
-    const ads = await Ads.filter(a => a.type === 'search')
-
-    for(let i = 0 ; i < ads.length ; i++){
-      const words = ads[i].keywords;
-      for(let j = 0 ; j < words.length ; j++){
-        if (q.includes(words[j])) {
-          relatedAds = ads[i]
-          break
-        }
-      }
-    }
-
-    relatedAds && engine.data.pop()
-    relatedAds && engine.data.unshift(relatedAds)
 
     res.status(200).json({ question: q, response: engine, summaryContent: parsedSources })
   } catch(e) {
