01253689b6b857ee0484317759dc398ca6df5c1b
handanawilliyantoro <handanawilli0902@gmail.com>
2023-04-17 20:35:48 +0700

    feat: add privacy policy page

---
+++ pages/privacy-policy.js
@@ -0,0 +1,83 @@
+import React from 'react'
+
+const PrivacyPolicy = () => {
+  return (
+    <div className='max-w-[70vw] p-4'>
+        <h1 className='text-xl font-bold font-serif my-2'>Privacy Policy</h1>
+        <h2 className='text-lg font-serif my-2'>Privacy policy for Handana AI</h2>
+        <p className='font-serif text-base my-2'>Handana AI accessible from https://handana.io, one of our main priorities is the privacy of our visitors.<br/>This Privacy Policy document contains types of information that is collected and recorded by Handana AI and how we use it.</p>
+        <p className='font-serif text-base my-2'>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
+        <p className='font-serif text-base my-2'>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in xoom internet. This policy is not applicable to any information collected offline or via channels other than this website. Our Privacy Policy was created with the help of the Privacy Policy Generator.</p>
+        <h2 className='text-lg font-serif my-2 mt-8 font-bold'>Consent</h2>
+        <p className='font-serif text-base my-2'>By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
+        <h2 className='text-lg font-serif my-2 mt-8 font-bold'>Information we collect</h2>
+        <p className='font-serif text-base my-2'>The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.</p>
+        <p className='font-serif text-base my-2'>If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.</p>
+        <p className='font-serif text-base my-2'>When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.</p>
+        <h2 className='text-lg font-serif my-2 mt-8 font-bold'>How we use your information</h2>
+        <p className='font-serif text-base my-2'>We use the information we collect in various ways, including to:</p>
+        <ol>
+            <li className='font-serif text-sm my-1'>
+                -. Provide, operate, and maintain our website
+            </li>
+            <li className='font-serif text-sm my-1'>
+                -. Improve, personalize, and expand our website
+            </li>
+            <li className='font-serif text-sm my-1'>
+                -. Understand and analyze how you use our website
+            </li>
+            <li className='font-serif text-sm my-1'>
+                -. Develop new products, services, features, and functionality
+            </li>
+        </ol>
+        <h2 className='text-lg font-serif my-2 mt-8 font-bold'>Log Files</h2>
+        <p className='font-serif text-base my-2'>Handana AI follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
+        <h2 className='text-lg font-serif my-2 mt-8 font-bold'>Advertising Partner</h2>
+        <p className='font-serif text-base my-2'>Some of advertisers on our site may use cookies and web beacons. Our advertising partners are listed below. Each of our advertising partners has their own Privacy Policy for their policies on user data. For easier access, we hyperlinked to their Privacy Policies below.</p>
+        <p onClick={() => window.open('https://policies.google.com/technologies/ads', '_blank')} className='font-serif cursor-pointer text-base my-2 hover:underline'>Google: https://policies.google.com/technologies/ads</p>
+        <h2 className='text-lg font-serif my-2 mt-8 font-bold'>Advertising Partners Privacy Policies</h2>
+        <p className='font-serif text-base my-2'>Note that xoom internet has no access to or control over these cookies that are used by third-party advertisers.</p>
+        <h2 className='text-lg font-serif my-2 mt-8 font-bold'>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
+        <p className='font-serif text-base my-2'>Under the CCPA, among other rights, California consumers have the right to:</p>
+        <ol>
+            <li className='font-serif text-sm my-1'>
+                -. Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.
+            </li>
+            <li className='font-serif text-sm my-1'>
+                -. Request that a business delete any personal data about the consumer that a business has collected.
+            </li>
+            <li className='font-serif text-sm my-1'>
+                -. Request that a business that sells a consumer's personal data, not sell the consumer's personal data.
+            </li>
+            <li className='font-serif text-sm my-1'>
+                -. If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+            </li>
+        </ol>
+        <h2 className='text-lg font-serif my-2 mt-8 font-bold'>GDPR Data Protection Rights</h2>
+        <p className='font-serif text-base my-2'>We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</p>
+        <ol>
+            <li className='font-serif text-sm my-1'>
+                -. The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.
+            </li>
+            <li className='font-serif text-sm my-1'>
+                -. The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.
+            </li>
+            <li className='font-serif text-sm my-1'>
+                -. The right to erasure – You have the right to request that we erase your personal data, under certain conditions.
+            </li>
+            <li className='font-serif text-sm my-1'>
+                -. The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.
+            </li>
+            <li className='font-serif text-sm my-1'>
+                -. The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.
+            </li>
+            <li className='font-serif text-sm my-1'>
+                -. The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.
+            </li>
+        </ol>
+        <p className='font-serif text-base my-2'>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
+    </div>
+  )
+}
+
+export default PrivacyPolicy
