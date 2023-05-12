ca97a2e7b4ca1bece9318abf69b714bd7f78ad9f
handanawilliyantoro <handanawilli0902@gmail.com>
2023-05-13 01:15:20 +0700

    feat: add response to http notification

--- pages/api/payment/http-notification.js
+++ pages/api/payment/http-notification.js
@@ -1,6 +1,7 @@
 export async function handler (req, res) {
     try {
         console.log(req.body, req.headers);
+        res.status(200).json({data: req.body, headers: req.headers, text: 'http notification'})
     } catch(e) {
         console.log(e)
         res.status(500).json({text: e.message, code: 500})
