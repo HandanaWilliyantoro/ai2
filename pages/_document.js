07bd1721a4f04274acb1694448021d1943a71dbb
handanawilliyantoro <handanawilli0902@gmail.com>
2023-05-28 23:54:12 +0700

    feat: plant google tag

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,14 @@ export default function Document() {
         src={process.env.MIDTRANS_PRODUCTION_ENV === "true" ? "https://app.midtrans.com/snap/snap.js" : "https://app.sandbox.midtrans.com/snap/snap.js"}
         data-client-key={process.env.MIDTRANS_CLIENT_KEY} 
       />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10945532713"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', 'AW-10945532713');
+      </script>
     </Html>
   )
 }
