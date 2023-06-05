635cacd8e2ecb7215874cb085d03a46922c38110
handanawilliyantoro <handanawilli0902@gmail.com>
2023-06-05 23:41:15 +0700

    fix: tai anjing

--- util/menus.js
+++ util/menus.js
@@ -1,4 +1,6 @@
-import {RxMagnifyingGlass, RxChatBubble, RxPencil2, RxImage} from 'react-icons/rx'
+import {RxMagnifyingGlass, RxChatBubble, RxPencil2, RxImage, RxMagicWand, RxRocket} from 'react-icons/rx'
+import {BsPlugin, BsApp, BsBoxArrowInDown} from 'react-icons/bs'
+import {MdWebhook} from 'react-icons/md'
 
 const menus = [
     {
@@ -25,6 +27,31 @@ const menus = [
         icon: <RxPencil2 className='w-3 h-3' />,
         premium: false
     },
+    {
+        label: 'Market',
+        slug: '/marketplace',
+        icon: <RxRocket className='w-3 h-3' />,
+        premium: false
+    },
+]
+
+const market_category_menus = [
+    {
+        label: 'My Apps',
+        icon: <BsBoxArrowInDown className='w-5 h-5' />,
+    },
+    {
+        label: 'Plugin',
+        icon: <BsPlugin className='w-5 h-5' />,
+    },
+    {
+        label: 'Application',
+        icon: <BsApp className='w-5 h-5' />
+    },
+    {
+        label: 'Webhook',
+        icon: <MdWebhook className='w-5 h-5' />
+    }
 ]
 
 const writeOptions = [
